0b716d0d8fa9b54288a119bf7f4d1886dc316519
anza19 <anza.k.9539@gmail.com>
2019-08-07 15:30:47 -0400

    Other stuff.

--- src/oop/InheritanceTraits.scala
+++ src/oop/InheritanceTraits.scala
@@ -1,5 +1,29 @@
 package oop
 
-object InheritanceTraits {
+object InheritanceTraits extends App {
 
+  class Animal {
+    def eat = println("nomnom");
+  }
+
+  class Cat extends Animal{
+    def crunch = {
+      eat
+      println("crunch crunch");
+    }
+  }
+  val cat = new Cat
+  cat.eat
+  cat.crunch
+
+  //constructors
+  class Person(name: String, age: Int)
+  class Adult(name: String, age: Int, idCard: String) extends Person(name, age)
+
+  class Dog extends Animal{
+    override def eat = println("crunch, crunch")
+  }
+  val dog = new Dog
+  dog.eat
+  println(dog.crea)
 }
